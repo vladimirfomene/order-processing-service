25587175e7d9415f93ca38a7819c3f357c328c00
vladimirfomene <vladimirfomene@gmail.com>
2020-08-14 13:52:16 +0100

    implement processRestock

--- order-service.js
+++ order-service.js
@@ -1,9 +1,17 @@
+const inventory = {};
+
 function initCatalog(productInfo) {
-  let inventory = {};
   for (let product of productInfo) {
     product.quantity = 0;
     inventory[product.product_id] = product;
   }
+}
 
-  return inventory;
+function processRestock(restock) {
+  for (let stock of restock) {
+    inventory[stock.product_id].quantity =
+      inventory[stock.product_id] === undefined
+        ? stock.quantity
+        : stock.quantity + inventory[stock.product_id].quantity;
+  }
 }
