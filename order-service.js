110ae845558ee75bf23c1a50e1db9b79b025b7a3
vladimirfomene <vladimirfomene@gmail.com>
2020-08-14 14:10:08 +0100

    implement the stub function shipPackage

--- order-service.js
+++ order-service.js
@@ -7,11 +7,22 @@ function initCatalog(productInfo) {
   }
 }
 
+//re-examine this method
 function processRestock(restock) {
   for (let stock of restock) {
     inventory[stock.product_id].quantity =
       inventory[stock.product_id] === undefined
         ? stock.quantity
         : stock.quantity + inventory[stock.product_id].quantity;
+  }
+}
+
+function shipPackage(shipment) {
+  console.log(`Order ${shipment.order_id} has just been shipped.`);
+  console.log(`It comprises of the following products: `);
+  for (let product of shipment.shipped) {
+    console.log(
+      `${product.quantity} ${inventory[product.product_id].product_name}`
+    );
   }
 }
